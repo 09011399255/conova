df75851e057229cdbce608c9145eb6a0c7f1349d
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-04-14 11:14:49 +0100

    Implemented all authentication flow

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,160 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
+from django.contrib.auth import get_user_model
+from core.models import ConovaUser
+
+
+User = get_user_model()
+
+
+class ConovaCreateUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        style={"input_field": "password"},
+    )
+
+    re_password = serializers.CharField(
+        write_only=True,
+        style={"input_field": "password"},
+    )
+
+    def validate(self, attrs):
+        password = attrs["password"]
+        confirm_password = attrs["re_password"]
+        email = attrs["email"]
+        attrs["is_active"] = False
+
+        if password != confirm_password:
+            raise serializers.ValidationError(
+                {"message": "Passwords do not match"},
+            )
+        try:
+            validate_password(password, user=User(email=email))
+        except DjangoValidationError as e:
+            raise serializers.ValidationError({"password": list(e.messages)})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("re_password")
+        return ConovaUser.objects.create_user(**validated_data)
+
+    class Meta:
+        model = ConovaUser
+        fields = (
+            "id",
+            "full_name",
+            "role",
+            "email",
+            "password",
+            "re_password",
+        )
+
+
+class ConovaUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConovaUser
+        fields = (
+            "id",
+            "full_name",
+            "email",
+            "role",
+            "qr_code_image",
+        )
+
+
+class ConovaActivateUserSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    otp = serializers.CharField(required=True)
+
+    def validate_email(self, data):
+        return data.strip().lower()
+
+
+class ConovaResetPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+    def validate_email(self, data):
+        return data.strip().lower()
+
+
+class ConovaPasswordResetConfirmSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    new_password = serializers.CharField(
+        write_only=True,
+        style={"input_field": "password"},
+    )
+    re_new_password = serializers.CharField(
+        write_only=True,
+        style={"input_field": "password"},
+    )
+    otp = serializers.CharField()
+
+    def validate(self, attrs):
+        password = attrs["new_password"]
+        confirm__password = attrs["re_new_password"]
+
+        if password != confirm__password:
+            raise serializers.ValidationError(
+                {"message": "Passwords do not match."},
+            )
+        try:
+            validate_password(password)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError({"password": list(e.messages)})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("re_new_password")
+        return super().create(validated_data)
+
+
+class ConovaPasswordChangeSerializer(serializers.Serializer):
+    current_password = serializers.CharField(
+        write_only=True,
+        style={"input_field": "password"},
+    )
+    new_password = serializers.CharField(
+        write_only=True,
+        style={"input_field": "password"},
+    )
+    re_new_password = serializers.CharField(
+        write_only=True,
+        style={"input_field": "password"},
+    )
+
+    def validate(self, attrs):
+        current_password = attrs["current_password"]
+        password = attrs["new_password"]
+        re_password = attrs["re_new_password"]
+
+        if password != re_password:
+            raise serializers.ValidationError(
+                {"message": "Passwords do not match."},
+            )
+        if password == current_password:
+            raise serializers.ValidationError(
+                {"message": "Current password can't be the same with new password."},
+            )
+        try:
+            validate_password(password)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError({"password": list(e.messages)})
+
+        return attrs
+
+    def validate_current_password(self, data):
+        user = self.context["request"].user
+        if not user.check_password(data):
+            raise serializers.ValidationError({"message": "Invalid credentials."})
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop("re_new_password")
+        return super().create(validated_data)
+
+    def save(self, **kwargs):
+        user = self.context["request"].user
+        user.set_password(self.validated_data["new_password"])
+        user.save()
+        return user
--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,12 +1,17 @@
+import os
+import secrets
+import qrcode
+from io import BytesIO
+from django.core.files import File
 from django.core.cache import cache
 from django.conf import settings
-import secrets
+import qrcode.constants
 
 
 def generate_otp(email):
     email = email.strip().lower()
     otp = f"{secrets.randbelow(10**6):06d}"
-    cache.set(f"otp_{email}", otp, timeout=300)
+    cache.set(f"otp_{email}", otp, timeout=600)
     return otp
 
 
@@ -28,3 +33,34 @@ def set_cookies(response, key, value):
         samesite="Lax",
         secure=secure_flag,
     )
+    
+def generate_qr_code(url):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=12,
+        border=2,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill="black", back_color="white")
+    
+    #save to memory buffer
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    buffer.seek(0)
+    return File(buffer, name="qr_code.png")
+
+
+def rename_avatar(instance, filename):
+    ext = os.path.splitext(filename)[1]
+    id = instance.id
+    filename = f"user_{id}{ext}"
+    return os.path.join("Avatars", filename)
+
+def rename_qr_code(instance, filename):
+    ext = os.path.splitext(filename)[1]
+    id = instance.id
+    filename = f"user_{id}{ext}"
+    return os.path.join("QRcodes", filename)
