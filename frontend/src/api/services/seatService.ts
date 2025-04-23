2459be67eed28d6d45afc96f13b2258963da43f9
Sultan A. Dev <sultanabaniks@gmail.com>
2025-04-23 19:59:23 +0100

    feat: add QR code generation and booking functionality in Activities and Spaces components

--- frontend/src/api/services/seatService.ts
+++ frontend/src/api/services/seatService.ts
@@ -2,3 +2,9 @@ import apiFetchWrapper from "../apiFetchWrapper";
 import { Seat } from "../types/seatsTypes";
 
 export const getSeats = () => apiFetchWrapper<Seat[]>('/seats/');
+
+export const bookSeat = (seatId: string) =>
+    apiFetchWrapper(`/seats/${seatId}/`, {
+        method: "PATCH",
+        body: { is_available: false },
+    });
